564375309660f615de8dd94de1d8de0984d43185
Salma Walid <salmawaliddd41@gmail.com>
2026-07-27 13:42:13 +0300

    Add lead filtering, search, sorting and pagination

---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://keystone.interns.addressdev.com',
+    ],
+
+    'allowed_origins_patterns' => ['/^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/'],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
